1bdf95cb8d1c4a0719e08a200a68fd81b1d27c7a
MARTY FABIEN <fabien.marty@meteo.fr>
2020-04-06 10:13:39 +0200

    build: better packaging of git dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
 import fastentrypoints  # noqa: F401
+import sys
 from setuptools import setup
 from setuptools import find_packages
 
+required = []
+dependency_links = []
+EGG_MARK = '#egg='
 with open('requirements.txt') as reqs:
-    install_requires = [
-        line for line in reqs.read().split('\n')
-        if (line and not line.startswith('--')) and (";" not in line)
-        and not line.startswith('git+https')]
+    for line in reqs.read().split('\n'):
+        if line.startswith('-e git:') or line.startswith('-e git+') or \
+                line.startswith('git:') or line.startswith('git+'):
+            if EGG_MARK in line:
+                package_name = line[line.find(EGG_MARK) + len(EGG_MARK):]
+                required.append(package_name)
+                dependency_links.append(line)
+            else:
+                print('Dependency to a git repository should have the format:')
+                print('git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name')
+                sys.exit(1)
+        else:
+            required.append(line)
 
 setup(
     name='mfplugin',
     packages=find_packages(),
-    install_requires=install_requires,
+    install_requires=required,
     include_package_data=True,
+    dependency_links=dependency_links,
     entry_points={
         "console_scripts": [
             "plugin_wrapper = mfplugin.cli_tools.plugin_wrapper:main",
