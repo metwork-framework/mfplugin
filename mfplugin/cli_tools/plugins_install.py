c23e2852fa2cc70793bc39fd6f304fcd876e6589
thebaptiste <jean-baptiste.veslin@meteo.fr>
2024-10-08 12:01:23 +0000

    build: other fixes

--- mfplugin/cli_tools/plugins_install.py
+++ mfplugin/cli_tools/plugins_install.py
@@ -92,14 +92,15 @@ def main():
     stderr = f.getvalue()
     if stderr != '':
         echo_warning()
-        if "pip's dependency resolver does not currently take into account" in \
-                stderr:
+        if "pip's dependency resolver does not currently take into account" \
+                in stderr:
             print(stderr.replace("ERROR", "WARNING"))
             print("The above message is only a WARNING, don't panic !")
             print("Your plugin should work anyway")
-            print("To get rid of it, maybe you should remove from your plugin the optional")
-            print("    layers (those starting by '-' in .layerapi2_dependencies)")
-            print("    or ask for help from a Metwork specialist")
+            print("To get rid of it, maybe you should remove from your plugin ")
+            print("    the optional layers (those starting by '-' in ")
+            print("    .layerapi2_dependencies or ask for help from a Metwork")
+            print("    specialist")
         else:
             print(stderr)
     else:
